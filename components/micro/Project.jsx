9b0a83829a7cbedb447050591c697fa8cce7e46d
Vivek <starktestic@gmail.com>
2022-01-19 20:19:43 +0530

    fixed bugs

--- components/micro/Project.jsx
+++ components/micro/Project.jsx
@@ -12,18 +12,12 @@ export default function Project() {
                 className="h-64 bg-cover lg:rounded-lg lg:h-full"
                 style={{
                   backgroundImage: `url(${item.imgUrl})`,
-                  // backgroundImage:
-                  //   "url('https://images.unsplash.com/photo-1593642532400-2682810df593?ixlib=rb-1.2.1&ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80')",
                 }}
               ></div>
             </div>
 
             <div className="max-w-xl px-6 py-12 lg:max-w-5xl lg:w-1/2">
               <h2 className="text-2xl font-bold text-gray-800 dark:text-white md:text-3xl">
-                {/* Build Your New{' '}
-                <span className="text-indigo-600 dark:text-indigo-400">
-                Idea
-              </span> */}
                 {item.title}
               </h2>
               <p className="mt-4 text-gray-600 dark:text-gray-400">
