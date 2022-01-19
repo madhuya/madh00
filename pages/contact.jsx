4a1aa7de9db418638f6f609146263559bf40fc8f
Vivek <starktestic@gmail.com>
2022-01-20 01:12:39 +0530

    deploy

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,14 +1,13 @@
-import Container from "../components/Container";
-import Contact from "../components/contact";
-
+import Container from '../components/Container';
+import Contact from '../components/micro/contact';
 
 export default function ContactPage() {
-    return (
-      <Container
-        title="Vivek Kumar - Developer, Entrepreneur and Memer"
-        description="I develope Websites, Mobile Apps. Get in touch with me to know more."
-      >
-        <Contact/>
-      </Container>
-    );
-  }
+  return (
+    <Container
+      title="Vivek Kumar - Developer, Entrepreneur and Memer"
+      description="I develope Websites, Mobile Apps. Get in touch with me to know more."
+    >
+      <Contact />
+    </Container>
+  );
+}
