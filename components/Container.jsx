02046fcbb20c624ce17bd83e1333e2456e54edff
Vivek <starktestic@gmail.com>
2022-01-20 11:39:47 +0530

    fix

--- components/Container.jsx
+++ components/Container.jsx
@@ -4,8 +4,6 @@ import { useRouter } from 'next/router';
 import Navbar from './Navbar';
 
 export default function ContainerBlock({ children, ...customMeta }) {
-  const router = useRouter();
-
   const meta = {
     title: 'Vivek Kumar - Developer, Entrepreneur, Creator and Memer',
     description: `I develope Websites, Mobile Apps. Get in touch with me to know more.`,
@@ -20,19 +18,6 @@ export default function ContainerBlock({ children, ...customMeta }) {
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
         <link rel="shortcut icon" href="/me.svg" />
-        {/* <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
-        <meta name="twitter:title" content={meta.title} />
-        <meta name="twitter:description" content={meta.description} />
-        <meta name="twitter:image" content={meta.image} />
-        {meta.date && (
-          <meta property="article:published_time" content={meta.date} />
-        )} */}
       </Head>
       <main className="dark:bg-gray-800  w-full h-screen">
         <Navbar />
--- components/Home.jsx
+++ components/Home.jsx
@@ -3,7 +3,7 @@ import userData from '../constants/data';
 export default function Home() {
   return (
     <section className="bg-white dark:bg-gray-800">
-      <div className="flex text-center bg-white relative">
+      <div className="flex text-cent relative">
         <div className="flex relative w-full h-screen items-center">
           <div className="container mx-auto px-6 flex flex-col justify-between items-center relative py-4">
             <div className="flex flex-col">
@@ -29,28 +29,6 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/* <div className="hidden md:block absolute -bottom-56 -left-32 w-96 h-96">
-          <div className="absolute text-extrabold text-xl right-16 z-20 text-start top-1/4">
-            <span className="text-7xl">
-              <img src="https://img.icons8.com/fluency/96/000000/idea.png" />
-            </span>
-            <p>Got a project ?</p>
-            <a href="mailto:starktestic@gmail.com" className="underline">
-              Let&#x27;s talk
-            </a>
-          </div>
-          <svg
-            viewBox="0 0 200 200"
-            className="absolute w-full h-full"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill="#FFDBB9"
-              d="M44.7,-76.4C58.8,-69.2,71.8,-59.1,79.6,-45.8C87.4,-32.6,90,-16.3,88.5,-0.9C87,14.6,81.4,29.2,74.1,43.2C66.7,57.2,57.6,70.6,45,78.1C32.4,85.6,16.2,87.1,0.7,85.9C-14.8,84.7,-29.6,80.9,-43.9,74.4C-58.3,67.9,-72,58.7,-79.8,45.9C-87.7,33,-89.5,16.5,-88.9,0.3C-88.4,-15.9,-85.4,-31.7,-78.1,-45.4C-70.8,-59.1,-59.1,-70.6,-45.3,-77.9C-31.6,-85.3,-15.8,-88.5,-0.3,-88.1C15.3,-87.6,30.5,-83.5,44.7,-76.4Z"
-              transform="translate(100 100)"
-            ></path>
-          </svg>
-        </div> */}
       </div>
     </section>
   );
