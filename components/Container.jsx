fb402f6abee374e069d686f6773d9c9f076783ed
Vivek <starktestic@gmail.com>
2022-01-20 11:40:36 +0530

    Update Container.jsx

--- components/Container.jsx
+++ components/Container.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import Navbar from './Navbar';
 
 export default function ContainerBlock({ children, ...customMeta }) {
