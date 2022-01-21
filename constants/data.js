a3ece2e34e9b4535deb3642e36a28c0d14a50866
Vivek <starktestic@gmail.com>
2022-01-21 22:54:56 +0530

    Update data.js

--- constants/data.js
+++ constants/data.js
@@ -12,7 +12,7 @@ const userData = {
     description: [
       `After learning HTML and struggling with CSS and Javascript, I started making frontend, but something was missing, its soul, so i learned python and Django. That was my first stack, Since then i never stopped learning`,
       `I've been coding in React, Node.js, Express, MongoDB, Bootstrap, Tailwind, C, C++, Rust, Although I barely know the syntax (Psst, Stack overflow!).`,
-      `I also have have worked with Machine Learnin and Deep Learning. Now i'm learning blockchain (DAPPs) development.`,
+      `I also have have worked with Machine Learning and Deep Learning. Now i'm learning blockchain (DAPPs) development.`,
     ],
     currentProject: 'Youcert',
     currentProjectUrl: 'https://github.com/l-fifa-l/OPenEd',
