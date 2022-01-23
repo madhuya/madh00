ad547ab56ca93220373597e7e9ec697750e4e2ea
Vivek <starktestic@gmail.com>
2022-01-23 13:32:29 +0530

    Update socialLinks.js

--- constants/socialLinks.js
+++ constants/socialLinks.js
@@ -1,28 +1,33 @@
 const socialLinks = [
   {
-    name: "Facebook",
-    href: "https://www.facebook.com/FxIxFxA/",
-    link: "https://img.icons8.com/fluency/50/000000/facebook-new.png",
+    name: 'Facebook',
+    href: 'https://www.facebook.com/FxIxFxA/',
+    link: 'https://img.icons8.com/fluency/50/000000/facebook-new.png',
   },
   {
-    name: "Twitter",
-    href: "https://twitter.com/l__fifa__l",
-    link: "https://img.icons8.com/fluency/48/000000/twitter.png",
+    name: 'Twitter',
+    href: 'https://twitter.com/l__fifa__l',
+    link: 'https://img.icons8.com/fluency/48/000000/twitter.png',
   },
   {
-    name: "Instagram",
-    href: "https://www.instagram.com/l_fifa_l/",
-    link: "https://img.icons8.com/fluency/48/000000/instagram-new.png",
+    name: 'Instagram',
+    href: 'https://www.instagram.com/l_fifa_l/',
+    link: 'https://img.icons8.com/fluency/48/000000/instagram-new.png',
   },
   {
-    name: "LinkedIn",
-    href: "#",
-    link: "https://img.icons8.com/fluency/48/000000/linkedin.png",
+    name: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/fifa/',
+    link: 'https://img.icons8.com/fluency/48/000000/linkedin.png',
   },
   {
-    name: "Mail",
-    href: "mailto:starktestic@gmail.com",
-    link: "https://img.icons8.com/fluency/48/000000/email-sign.png",
+    name: 'GitHub',
+    href: 'https://github.com/l-fifa-l',
+    link: 'https://img.icons8.com/glyph-neue/64/000000/github.png',
+  },
+  {
+    name: 'Mail',
+    href: 'mailto:starktestic@gmail.com',
+    link: 'https://img.icons8.com/fluency/48/000000/email-sign.png',
   },
 ];
 
